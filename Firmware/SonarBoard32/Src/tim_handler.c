b44b27b309824e34ba1d1745f9aea141c9b52d0f
Walter Lucetti <myzhar@robot-home.it>
2016-02-11 14:48:03 +0100

    Timer handler fixed

--- Firmware/SonarBoard32/Src/tim_handler.c
+++ Firmware/SonarBoard32/Src/tim_handler.c
@@ -17,6 +17,48 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 // Callback for sonar echo
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
-  if( htim==&htim1 || htim==&htim2 || htim==&htim3 || htim==&htim15 )
-    onNewMeasureReceived( htim );
+  /*if( htim->Instance == TIM1 ||
+      htim->Instance == TIM2 ||
+      htim->Instance == TIM3 ||
+      htim->Instance == TIM15 )
+    onNewMeasureReceived( htim );*/
+  
+  int idx;
+  
+  // >>>>> Which sonar has received the echo?
+  if( htim->Instance == TIM1 )
+  {
+    idx=SONAR_0;
+    elapsed_tick[idx] = HAL_GetTick() - trigger_tick[COUPLE_0_2];
+  }
+  else if( htim->Instance == TIM2 )
+  {
+    idx=SONAR_1;
+    elapsed_tick[idx] = HAL_GetTick() - trigger_tick[COUPLE_0_2];
+  }
+  else if( htim->Instance == TIM3 )
+  {
+    idx=SONAR_2;
+    elapsed_tick[idx] = HAL_GetTick() - trigger_tick[COUPLE_1_3];
+  }
+  else if( htim->Instance == TIM15 )
+  {
+    idx=SONAR_3;
+    elapsed_tick[idx] = HAL_GetTick() - trigger_tick[COUPLE_1_3];
+  } 
+  else 
+    return; // Should never be here!!!
+  // <<<<< Which sonar has received the echo?
+  
+  // Update Input Capture times 
+  uint32_t ic_val = HAL_TIM_ReadCapturedValue( htim, TIM_CHANNEL_2 ); 
+  
+  // >>>>> Durations update
+  if( ic_val > MAX_DURATION_10_USEC )
+    distValid[idx] = 0;
+  else
+    distValid[idx] = 1;
+  
+  echo_duration[idx]=ic_val;
+  // <<<<< Durations update
 }
